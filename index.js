56a01559a4b226053b2abbd76a08ab80c854e628
kumardevsingh <kumardevsingh@gmail.com>
2021-03-13 15:58:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const merge = (leftArr, rightArr) => {
+    const output = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+        const leftEl = leftArr[leftIndex];
+        const rightEl = rightArr[rightIndex];
+
+        if (leftEl < rightEl) {
+            output.push(leftEl);
+            leftIndex++;
+        } else {
+            output.push(rightEl);
+            rightIndex++;
+        }
+    }
+
+    return [...output, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)]
+}
+
+//console.log(merge([29, 30], [8, 19]));
+
+const megeSort = (array) => {
+    if (array.length <= 1) {
+        return array;
+    }
+    const middleIndex = Math.floor(array.length / 2);
+    const leftArr = array.slice(0, middleIndex);
+    const rightArr = array.slice(middleIndex);
+
+    return merge(
+        megeSort(leftArr),
+        megeSort(rightArr)
+    );
+}
+
+const array = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+
+console.log(megeSort(array));
